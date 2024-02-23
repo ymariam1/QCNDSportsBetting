7b9a62e43c38c6e3ca46f44b7f76994b9c9703c3
Yohannes Mariam <yohannesjrmariam@gmail.com>
2024-02-23 00:33:48 -0500

    Selected a website for scraping, began scraping process

--- main.py
+++ main.py
@@ -7,4 +7,11 @@ import matplotlib.pyplot as plt
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+driver = webdriver.Chrome()
+driver.get("https://unabated.com/nba/props")
+title = driver.title
+driver.implicitly_wait(1)
+prop_names = driver.find_element(by=By.CLASS_NAME, value="btn-sm btn-falcon-primary dropdown-toggle btn btn-secondary")
+prop_names.click()
+print("Complete")
 
