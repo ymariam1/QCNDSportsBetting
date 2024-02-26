68fae45e85decdc7537d387f99d3152ea1edf40b
Yohannes Mariam <yohannesjrmariam@gmail.com>
2024-02-26 00:08:22 -0500

    Rename df, remove team names

--- helper.py
+++ helper.py
@@ -1,6 +1,3 @@
-import math
-import numpy
-
 def devig(over, under):
     # Assumes decimal Odds
     over = float(over)
@@ -13,3 +10,14 @@ def devig(over, under):
     true_under = impU/(impO+impU)
     return [true_over, true_under]
 
+def store_data(player, prop, line, over, under, true_over, true_under):
+    # Append the data to the list as a dictionary
+    return {
+        'Player': player,
+        'Prop': prop,
+        'Line': line,
+        'Over': over,
+        'Under': under,
+        'True Over': true_over,
+        'True Under': true_under
+    }
--- main.py
+++ main.py
@@ -11,35 +11,32 @@ from selenium.webdriver.support import expected_conditions as EC
 from helper import *
 import time
 
-driver1 = webdriver.Chrome()
-# driver2 = webdriver.Chrome()
+driver = webdriver.Chrome()
 pinnacle_data = []
 # Scrape True Value Odds
-driver1.get("https://www.pinnacle.com/en/basketball/nba/matchups/#period:0")
-upcoming_games = driver1.find_elements(By.XPATH, '//div[@class="style_dateBar__1adEH"]/following-sibling::div[@class="style_row__yBzX8 style_row__12oAB"]')
+driver.get("https://www.pinnacle.com/en/basketball/nba/matchups/#period:0")
+upcoming_games = driver.find_elements(By.XPATH, '//div[@class="style_dateBar__1adEH"]/following-sibling::div[@class="style_row__yBzX8 style_row__12oAB"]')
 urls = []
 
 for game in upcoming_games:
     # Extract game information
     link_element = game.find_element(By.CLASS_NAME, 'style_btn__3I6l1')
-    # Extract the href attribute value and append it to the urls list
+    # Stores upcoming game links inside an array
     url = link_element.get_attribute('href')
     urls.append(url)
-    teams = game.find_elements(By.XPATH, './/span[@class="ellipsis event-row-participant style_participant__2BBhy"]')
-    team1 = teams[0].text
-    team2 = teams[1].text
 for url in urls:
-    driver1.get(url)
-    player_props_btn = WebDriverWait(driver1,10).until(
+    driver.get(url)
+    player_props_btn = WebDriverWait(driver,10).until(
     EC.element_to_be_clickable((By.ID,"player-props"))
 )
     player_props_btn.click()
-    main_cont = driver1.find_element("xpath",'/html/body/div[2]/div[1]/div[2]/main/div[3]')
+    main_cont = driver.find_element("xpath",'/html/body/div[2]/div[1]/div[2]/main/div[3]')
     div_elements = main_cont.find_elements("xpath",'.//div[@data-test-id="Collapse"]')
     for div_element in div_elements:
         # Extract player name and prop from the span element
         player_prop = div_element.find_element("xpath",'.//span').text
         player, prop = player_prop.split('(')
+        prop = prop[:-1]
 
         # Extract the buttons for Over and Under
         over_button = div_element.find_element("xpath",'.//button[contains(@title, "Over")]')
@@ -51,31 +48,9 @@ for url in urls:
         under = under_button.find_element("xpath",'.//span[@class="style_price__3Haa9"]').text
 
         [tOver,tUnder] = devig(over, under)
-        pinnacle_data.append(store_data(team1, team2, player_prop, line, over, under, tOver, tUnder))
-df = pd.DataFrame(pinnacle_data)
-print(df)
-
-# Click button to view bets on first NBA game
-# viewPropBtn = WebDriverWait(driver1, 10).until(
-#     EC.element_to_be_clickable((By.CLASS_NAME, "style_btn__3I6l1"))
-# )
-# viewPropBtn.click()
-#Clicks on button to view only player props
+        pinnacle_data.append(store_data(player, prop, line, over, under, tOver, tUnder))
+true_vals = pd.DataFrame(pinnacle_data)
+print(true_vals)
 
 
-
-#Looks at all the props and stores them in div_elements
-
-    # Append the extracted information to the DataFrame
-    #pinnacle_data.append({'Player': player.strip(), 'Prop': prop[:-1].strip(), 'Line': line, 'Over': over, 'Under': under, 'True Over': tOver, 'True Under': tUnder})
-
-# Print the DataFrame
-
-# Scrape all odds
-"""
-driver2.get("https://unabated.com/nba/props")
-driver2.implicitly_wait(1)
-prop_names = driver2.find_element(by=By.CLASS_NAME, value="btn-sm btn-falcon-primary dropdown-toggle btn btn-secondary")
-prop_names.click()
-print("Complete")
-"""
+driver.get("https://unabated.com/nba/props")
